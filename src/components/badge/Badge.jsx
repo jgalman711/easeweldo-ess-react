30994737ee1aca06385d5a274f7d81d038e46576
Jermaine Galman <jgalman711@gmail.com>
2024-01-24 02:29:41 +0800

    display timesheet

--- src/components/badge/Badge.jsx
+++ src/components/badge/Badge.jsx
@@ -3,9 +3,9 @@ const Badge = (props) => {
   return (
     <div
       className={`w-24 text-center rounded-sm py-[5px] text-xs font-bold uppercase text-white transition duration-200 ${
-        (state === "error" || state === "failed")
+        (state === "error" || state === "failed" || state === "absent")
           ? "bg-red-500 dark:bg-red-400"
-          : (state === "success" || state === "completed" || state === "approved")
+          : (state === "success" || state === "completed" || state === "approved" || state === "on-time")
           ? "bg-green-500 dark:bg-green-400"
           : state === "info"
           ? "bg-yellow-500 dark:bg-yellow-400"
---
+++ src/views/ess/timesheet/variables/headersData.js
@@ -0,0 +1,22 @@
+export const headersData = [
+  {
+    Header: "Date",
+    accessor: "expected_clock_in",
+  },
+  {
+    Header: "Clock In",
+    accessor: "clock_in",
+  },
+  {
+    Header: "Clock Out",
+    accessor: "clock_out",
+  },
+  {
+    Header: "Source",
+    accessor: "source",
+  },
+  {
+    Header: "Status",
+    accessor: "attendance_status",
+  },
+];
