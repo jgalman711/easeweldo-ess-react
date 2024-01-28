3cd827a8fd3001ea8423de4a2f443806c729b1a4
Jermaine Galman <jgalman711@gmail.com>
2024-01-28 21:25:39 +0800

    qr code for attendance

--- src/views/ess/qrGenerate/index.jsx
+++ src/views/ess/qrGenerate/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "components/card";
 import banner from "assets/img/profile/banner.png";
+import QrCodeGenerator from "components/qr/QrCodeGenerator";
 
 const QrGenerate = () => {
   const firstName = localStorage.getItem('firstName');
@@ -11,12 +12,12 @@ const QrGenerate = () => {
 
   return (
     <div className="flex items-center justify-center w-full">
-      <Card extra={"items-center w-full max-w-4xl p-[16px] bg-cover"}>
+      <Card extra={"items-center w-full max-w-xl p-[16px] bg-cover"}>
         <div
           className="relative mt-1 flex h-32 w-full justify-center rounded-xl bg-cover"
           style={{ backgroundImage: `url(${banner})` }}
         >
-          <div className="absolute -bottom-12 flex h-[165px] w-[165px] items-center justify-center rounded-full border-[4px] border-white bg-pink-400 dark:!border-navy-700">
+          <div className="absolute -bottom-12 flex h-[150px] w-[150px] items-center justify-center rounded-full border-[4px] border-white bg-pink-400 dark:!border-navy-700">
             <img className="h-full w-full rounded-full" src={storageUrl + profilePicture} alt="" />
           </div>
         </div>
@@ -25,6 +26,9 @@ const QrGenerate = () => {
             {firstName} {lastName}
           </h4>
           <p className="text-base font-normal text-gray-600">{jobTitle}</p>
+        </div>
+        <div className="my-1">
+          <QrCodeGenerator />
         </div>
       </Card>
     </div>
