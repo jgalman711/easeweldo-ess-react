b3002c18d20207a34813bc480de8ac83ce1a31f9
Jermaine Galman <jgalman711@gmail.com>
2024-01-30 00:35:23 +0800

    profile picture update

--- src/views/ess/profile/index.jsx
+++ src/views/ess/profile/index.jsx
@@ -1,5 +1,4 @@
 import BankInformation from "./components/BankInformation";
-import Banner from "./components/Banner";
 import IdentificationNumbers from "./components/IdentificationNumbers";
 import PersonalInformation from "./components/PersonalInformation";
 import WorkInformation from "./components/WorkInformation";
@@ -25,7 +24,6 @@ const ProfileOverview = () => {
   return (
     <div className="flex w-full grid grid-cols-1 md:grid-cols-2 gap-5">
       <div className="w-ful mt-3 flex flex-col gap-5">
-        <Banner data={data} />
         <PersonalInformation data={data} />
         <IdentificationNumbers data={data} />
         {/* <EmergencyContact data={data} /> */}
